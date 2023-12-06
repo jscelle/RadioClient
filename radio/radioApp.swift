609643a85c99f3455fadb2dd41c61d0fafb3015b
Artem Raykh <77747763+jscelle@users.noreply.github.com>
2023-12-06 16:47:50 +0300

    Initial Commit

---
+++ radio/radioApp.swift
@@ -0,0 +1,17 @@
+//
+//  radioApp.swift
+//  radio
+//
+//  Created by Artem Raykh on 06.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct radioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
